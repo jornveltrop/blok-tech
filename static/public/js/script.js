0af70cc8e688aab0c75459bcec79b5a188cabdee
jornveltrop <jorn1veltrop@gmail.com>
2021-02-15 12:55:21 +0100

    profile page coding

---
+++ static/public/js/script.js
@@ -0,0 +1,12 @@
+// Toggle favorite
+const starUnfilled = document.querySelector("header ul a:first-of-type");
+const starFilled = document.querySelector("header ul a:last-of-type");
+
+starUnfilled.addEventListener("click", favorite);
+starFilled.addEventListener("click", favorite);
+
+function favorite() {
+  starFilled.classList.toggle("display");
+  starUnfilled.classList.toggle("display");
+  console.log("TEST")
+};
