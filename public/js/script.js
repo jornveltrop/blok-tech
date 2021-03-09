8a4b2e42057e0977f98db746d0bb2e446efaf9bb
jornveltrop <jorn1veltrop@gmail.com>
2021-03-09 05:24:59 +0100

    Finishing documentation

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,8 @@
 // Preview uploaded profilePic
 function previewProfilePic() {
-  const prPic = document.querySelector("#prPic");
+  const prPic = document.querySelector('#prPic');
 
-  prPic.addEventListener("change", setPreviewProfilePic);
+  prPic.addEventListener('change', setPreviewProfilePic);
   
   function setPreviewProfilePic() {
     profilePic.src=URL.createObjectURL(event.target.files[0]);
@@ -12,25 +12,25 @@ function previewProfilePic() {
 
 // Toggle favorite
 function toggleFavorite() {
-  const starUnfilled = document.querySelector(".profile header ul a:first-of-type");
-  const starFilled = document.querySelector(".profile header ul a:last-of-type");
+  const starUnfilled = document.querySelector('.profile header ul a:first-of-type');
+  const starFilled = document.querySelector('.profile header ul a:last-of-type');
 
-  starUnfilled.addEventListener("click", toggleFavorite);
-  starFilled.addEventListener("click", toggleFavorite);
+  starUnfilled.addEventListener('click', toggleFavorite);
+  starFilled.addEventListener('click', toggleFavorite);
 
   function toggleFavorite() {
-    starFilled.classList.toggle("display");
-    starUnfilled.classList.toggle("display");
+    starFilled.classList.toggle('display');
+    starUnfilled.classList.toggle('display');
   }
 }
 
 
 // Activate scripts
-if (document.URL.indexOf("/profile/") > -1 ) { 
+if (document.URL.indexOf('/profile/') > -1 ) { 
   toggleFavorite();
 }
 
-if (document.URL.indexOf("/addProfile") > -1 ) { 
+if (document.URL.indexOf('/addProfile') > -1 ) { 
   previewProfilePic();
   toggleFavorite();
 }
